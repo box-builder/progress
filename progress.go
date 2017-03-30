7d6632d0a4215994a69273b6a8987f831bf7037c
Erik Hollensbe <github@hollensbe.org>
2017-03-30 02:58:48 -0700

    Reformat documentation example
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- progress.go
+++ progress.go
@@ -2,28 +2,30 @@
 //
 // Example:
 //
-//     func progress(reader io.Reader) {
-//     	r = NewReader("an_file.tar", reader, 100*time.Millisecond)
-//     	defer r.Close()
 //
-//     	var count uint64
-//     	// This goroutine consumes the channel and concatenates the count to display
-//     	// the total as it increases.
-//     	go func(r *Reader) {
-//     		for tick := range r.C {
-//     			count += tick.Value
-//     			fmt.Printf("\r%d", count)
-//     		}
+// func progress(reader io.Reader) {
+// 	r = NewReader("an_file.tar", reader, 100*time.Millisecond)
+// 	defer r.Close()
 //
-//     		fmt.Println()
-//     	}(r)
+// 	var count uint64
+// 	// This goroutine consumes the channel and concatenates the count to display
+// 	// the total as it increases.
+// 	go func(r *Reader) {
+// 		for tick := range r.C {
+// 			count += tick.Value
+// 			fmt.Printf("\r%d", count)
+// 		}
 //
-//     	// use r like you normally would; progress will be reported to C every 100
-//     	// milliseconds.
-//     	if _, err := io.Copy(ioutil.Discard, r); err != nil {
-//     		panic(err)
-//     	}
-//     }
+// 		fmt.Println()
+// 	}(r)
+//
+// 	// use r like you normally would; progress will be reported to C every 100
+// 	// milliseconds.
+// 	if _, err := io.Copy(ioutil.Discard, r); err != nil {
+// 		panic(err)
+// 	}
+// }
+//
 //
 //
 package progress
